f119b842c3fb2f32c3ef4dafae682980840ef110
AR-Shahin <rahman.anisur@sslwireless.com>
2025-08-10 11:23:35 +0600

    Security added in the socket server

--- index.js
+++ index.js
@@ -2,11 +2,17 @@ const http = require("http");
 const express = require("express");
 const { Server } = require("socket.io");
 const axios = require("axios");
+const jwt = require("jsonwebtoken");
+require("dotenv").config(); 
 
 const app = express();
 
 // Create HTTP server using Express app
 const server = http.createServer(app);
+const JWT_SECRET = process.env.JWT_SECRET || "TS9ICUzn2zDGKV4fdbIDsc92yoLce8P7NxcIPVXW4RmbukpEnQRVZMBOUvj0PUNJ";
+const BACKEND_URL = process.env.BACKEND_URL || "http://localhost:8000";
+const ROOM_ACCESS_URL = `${BACKEND_URL}/api/check-room-access`;
+
 
 // Pass HTTP server to Socket.IO
 const io = new Server(server, {
@@ -22,14 +28,53 @@ app.get("/", (req, res) => {
   });
 });
 
+// JWT auth middleware for Socket.IO connections
+io.use((socket, next) => {
+  const token = socket.handshake.auth.token;
+  if (!token) {
+    return next(new Error("Authentication error: Token required"));
+  }
+  
+  try {
+    const decoded = jwt.verify(token, JWT_SECRET);
+   
+    socket.user = decoded; 
+    console.log("JWT verified successfully:");
+    next();
+  } catch (err) {
+    console.log("JWT verification failed:", err.message);
+    return next(new Error("Authentication error: Invalid token"));
+  }
+});
+
 io.on("connection", (socket) => {
   console.log(`Client connected: ${socket.id}`);
 
-  socket.on("join-room", ({ chatRoomId }) => {
-    const roomName = `chat_room_${chatRoomId}`;
-    socket.join(roomName);
-    console.log(`Client ${socket.id} joined room: ${roomName}`);
-  });
+  socket.on("join-room", async ({ chatRoomId }) => {
+      const userId = socket.user.sub || socket.user.id; 
+      try {
+        const response = await axios.post(ROOM_ACCESS_URL, {
+          user_id: userId,
+          chat_room_id: chatRoomId,
+        }, {
+          headers: {
+            Authorization: `Bearer ${socket.handshake.auth.token}`, 
+          },
+        });
+        
+        if (response.data.allowed) {
+          const roomName = `chat_room_${chatRoomId}`;
+          socket.join(roomName);
+          console.log(`User ${userId} joined room: ${roomName}`);
+        } else {
+          //socket.emit("error", "Access denied to this chat room");
+          console.log(`User ${userId} denied access to room: ${chatRoomId}`);
+        }
+      } catch (err) {
+        console.error("Error checking room access:", err.message);
+        //socket.emit("error", "Could not verify room access");
+      }
+    });
 
   socket.on("send-message", async (data) => {
     const roomName = `chat_room_${data.chat_room_id}`;
